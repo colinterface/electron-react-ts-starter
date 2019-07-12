dfa2378add99705dafff2f63f7ed746471529617
Colin Parsons <casparsons@gmail.com>
2019-07-12 16:05:01 -0700

    🚀

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { IpcRenderer, IpcMessageEvent} from 'electron' ; 
-const electron  = window.require('electron') ;  // require electron like this in all the files. Don't Use import from 'electron' syntax for importing IpcRender from electron.
+import { IpcRenderer, IpcMessageEvent } from 'electron';
+const electron = window.require('electron');  // require electron like this in all the files. Don't Use import from 'electron' syntax for importing IpcRender from electron.
 
-let ipcRenderer : IpcRenderer  = electron.ipcRenderer ; 
+let ipcRenderer: IpcRenderer = electron.ipcRenderer;
 
-ipcRenderer.on('response' , (event:IpcMessageEvent , args:any)=>{
-  console.log(args);
+let dirs: string[] = [];
+ipcRenderer.on('response', (event: IpcMessageEvent, args: any) => {
+  console.log(args.asdf);
+  dirs = args.asdf;
 })
 
+
 const App: React.FC = () => {
+  console.log('dirs: ', dirs);
   return (
-    <div className="App">
-      <button onClick={e=>ipcRenderer.send('channel' , {title : 'hi' , content : 'hello this is my message'})} >
-        Click Him
+    <div>
+      <React.Fragment>
+        {dirs.map(dir => <h1>{dir}</h1>)}
+      </React.Fragment>
+      <button onClick={e => ipcRenderer.send('channel', { title: 'woo', content: 'yey' })} >
+        Click me
       </button>
     </div>
   );
